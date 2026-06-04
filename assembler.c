8b90a34b19508f65ee0b4a11d6810fb3771edfa8
smavanat <edwin.hadass@hotmail.com>
2026-06-04 22:36:57 +0100

    Added proper file loading and writing to the assembler

--- assembler.c
+++ assembler.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 void write_text(char *text, size_t len) {
     FILE *fptr = fopen("test.txt", "w");
@@ -6,8 +8,72 @@ void write_text(char *text, size_t len) {
     fclose(fptr);
 }
 
+int read_to_end(char const *path, char **buf, uint8_t add_null) {
+    FILE *fp;
+    size_t fsz;
+    long offEnd;
+    int rc;
+
+    //Open the file
+    fp = fopen(path, "rb");
+    if(NULL == fp) {
+        return -1;
+    }
+
+    //Seek to the end of the file
+    rc = fseek(fp, 0L, SEEK_END);
+    if(0 != rc) {
+        return -1;
+    }
+
+    //Byte offset to the end of the file size
+    if(0 > (offEnd = ftell(fp))) {
+        return -1;
+    }
+    fsz = (size_t)offEnd;
+
+    //Allocate a buffer to hold the whole file
+    *buf = malloc(fsz + (int)add_null);
+    if(NULL == *buf) {
+        return -1;
+    }
+
+    //Rewind file pointer to the start of the file:
+    rewind(fp);
+
+    //Place the file into a buffer
+    if(fsz != fread(*buf, 1, fsz, fp)) {
+        free(buf);
+        return -1;
+    }
+
+    //Close the file
+    if(EOF == fclose(fp)) {
+        free(*buf);
+        return -1;
+    }
+
+    //Add null terminator
+    if(add_null) {
+        buf[fsz] = "\0";
+    }
+
+    return fsz;
+}
+
+void read() {
+    FILE *fptr = fopen("input.txt", "r");
+    char *buf;
+
+    int len = read_to_end("input.txt", &buf, 1);
+    fclose(fptr);
+
+    write_text(buf, len);
+}
+
 int main(void) {
-    write_text("Hello World", 11);
+    // write_text("Hello World", 11);
+    read();
 
     return 0;
 }
